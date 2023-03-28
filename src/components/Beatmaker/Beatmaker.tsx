4650d8080eef871f0ca0e0571824ae99256f49ac
Artem Gordienko <morrag1986@gmail.com>
2023-03-28 18:48:38 +0600

    web audio api work

--- src/components/Beatmaker/Beatmaker.tsx
+++ src/components/Beatmaker/Beatmaker.tsx
@@ -1,32 +1,89 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import styles from './Beatmaker.module.scss';
-import bassUrl from '../../sounds/561593__panxozerok__safari-kick-2.wav';
+import bassUrl from '../../sounds/205072__thalamus_lab__tapping-amphora-11.wav';
+import snareUrl from '../../sounds/347323__newagesoup__thick-stamp-sub.wav';
 import BeatmakerSlot from './BeatmakerSlot/BeatmakerSlot';
 import { useStore } from '../../store/useStore';
 
 const Beatmaker = observer(function Beatmaker() {
+  const [audioContext, setAudioContext] = useState<AudioContext | null>(null);
+  const [sample, setSample] = useState<AudioBuffer | null>(null);
+  const [secSample, setSecSample] = useState<AudioBuffer | null>(null);
   const store = useStore();
-  const [tick, setTick] = useState<number>(0);
-  const audioBass = useMemo(() => new Audio(bassUrl), [bassUrl]);
-
   const slotsCount = 12;
-
   const tickSpeed = 150;
+  const [tick, setTick] = useState<number>(0);
 
-  const playBass = useCallback(() => {
-    console.log('bass');
-    audioBass.pause();
-    audioBass.currentTime = -3;
-    audioBass.playbackRate = 1;
-    audioBass.volume = 1;
-    audioBass.play();
+  // Getting file and converting it audio buffer
+  async function loadSample(filePath: string) {
+    const response = await fetch(filePath);
+    const arrayBuffer = await response.arrayBuffer();
+    const audioBuffer = await audioContext?.decodeAudioData(arrayBuffer);
+    return audioBuffer;
+  }
+
+  const loadBass = () => {
+    loadSample(bassUrl).then(resp => {
+      console.log(resp);
+      if (resp) {
+        setSample(resp);
+      }
+    });
+  };
+
+  const loadSecond = () => {
+    loadSample(snareUrl).then(resp => {
+      console.log(resp);
+      if (resp) {
+        setSecSample(resp);
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (!audioContext) {
+      setAudioContext(new AudioContext());
+    }
   }, []);
+
+  useEffect(() => {
+    if (audioContext && !sample) {
+      loadBass();
+      loadSecond();
+    }
+  }, [audioContext]);
+
+  function playBass(buffer) {
+    if (audioContext) {
+      const sampleSource = audioContext.createBufferSource();
+      sampleSource.buffer = buffer;
+      sampleSource.connect(audioContext.destination);
+      sampleSource.start();
+    }
+  }
+
+  function playSec(buffer) {
+    if (audioContext) {
+      const sampleSource = audioContext.createBufferSource();
+      sampleSource.buffer = buffer;
+      sampleSource.connect(audioContext.destination);
+      sampleSource.start();
+    }
+  }
+
+  const playSound = useCallback(() => {
+    playBass(sample);
+  }, [audioContext, sample]);
+
+  const playSnare = useCallback(() => {
+    playSec(secSample);
+  }, [audioContext, secSample]);
 
   useEffect(() => {
     const timer = setInterval(() => {
       if (store.playActive) {
-        setTick(prevTime => (tick < slotsCount ? prevTime + 1 : 0));
+        setTick(prevTime => (tick < slotsCount - 1 ? prevTime + 1 : 0));
       } else {
         setTick(-1);
       }
@@ -42,7 +99,21 @@ const Beatmaker = observer(function Beatmaker() {
         <BeatmakerSlot
           key={index}
           beatType='bass'
-          soundCallback={playBass}
+          soundCallback={playSound}
+          index={index}
+          play={index === tick}
+        />
+      )),
+    [slotsCount, tick, tickSpeed]
+  );
+
+  const renderSnare = useMemo(
+    () =>
+      [...Array(slotsCount)].map((e, index) => (
+        <BeatmakerSlot
+          key={index}
+          beatType='bass'
+          soundCallback={playSnare}
           index={index}
           play={index === tick}
         />
@@ -54,10 +125,16 @@ const Beatmaker = observer(function Beatmaker() {
     <div className={styles.beatmaker}>
       <button onClick={() => store.togglePlayActive()}>{store.playActive ? 'Stop' : 'Play'}</button>
       <div className={styles.beatmaker_row}>
-        <div className={styles.beatmaker_previewButton} onClick={() => playBass()}>
+        <div className={styles.beatmaker_previewButton} onClick={() => {}}>
           Bass
         </div>
         {renderBass}
+      </div>
+      <div className={styles.beatmaker_row}>
+        <div className={styles.beatmaker_previewButton} onClick={() => {}}>
+          Bass
+        </div>
+        {renderSnare}
       </div>
     </div>
   );
