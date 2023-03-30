4908ed971777468b922d6d3739ada991b7b5ca7f
Artem Gordienko <morrag1986@gmail.com>
2023-03-30 13:22:39 +0600

    clear

--- src/components/BeatSelect/BeatSelect.tsx
+++ src/components/BeatSelect/BeatSelect.tsx
@@ -1,10 +1,6 @@
 function BeatSelect() {
-  const handleOnSelect = e => {
-    console.log(e.target.value);
-  };
-
   return (
-    <select onChange={handleOnSelect} defaultValue='123'>
+    <select defaultValue='123'>
       <option value='hero'>Один</option>
       <option value='as'>Два</option>
       <option value='123'>Три</option>
